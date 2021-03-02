5ca47920ea6cfaae1669f4fcf1d48945de682846
ibrahim altay <ibrahimaltay152@hotmail.com>
2021-03-02 23:29:32 +0300

    Updated email

--- app.py
+++ app.py
@@ -5,18 +5,18 @@ from models import fetch_btc_data_from_binance, create_btc_table_if_not_exists, 
 create_altcoin_table_if_not_exists, fetch_altcoin_data_from_binance
 
 def main():
-    send_email("Bot is up and running!", "SUCCESS", "xrge152@gmail.com")
+    
+    send_email("Bot is up and running!", "SUCCESS", "xrge152@gmail.com, sinatalay@hotmail.com")
     
     print("Application Started!")
+    
     create_btc_table_if_not_exists()
     create_altcoin_table_if_not_exists()
     run_method_with_perfect_intervals(fetch_btc_data_from_binance, 1)
-    
-        
-
 
 if __name__ == "__main__":
+    
     try:
         main()
     except Exception as e:
-        send_email("The application stopped: " + str(e))
+        send_email("The application stopped: " + str(e), "FAIL", "xrge152@gmail.com, sinatalay@hotmail.com")
