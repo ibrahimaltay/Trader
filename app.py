be3d7069524daa29109a0c335b3eaf849d105692
ibrahim altay <ibrahimaltay152@hotmail.com>
2021-03-02 23:23:13 +0300

    Updated

--- app.py
+++ app.py
@@ -6,15 +6,17 @@ create_altcoin_table_if_not_exists, fetch_altcoin_data_from_binance
 
 def main():
     send_email("Bot is up and running!", "SUCCESS", "xrge152@gmail.com")
-
-    try:
-        print("Application Started!")
-        create_btc_table_if_not_exists()
-        create_altcoin_table_if_not_exists()
-        run_method_with_perfect_intervals(fetch_btc_data_from_binance, 1)
-    except Exception as e:
-        send_email("The application stopped: " + str(e))
+    
+    print("Application Started!")
+    create_btc_table_if_not_exists()
+    create_altcoin_table_if_not_exists()
+    run_method_with_perfect_intervals(fetch_btc_data_from_binance, 1)
+    
+        
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        send_email("The application stopped: " + str(e))
