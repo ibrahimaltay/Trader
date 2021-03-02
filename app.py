c1c6deb34b5b650a5927b2b11830d5aa841ae757
ibrahim altay <ibrahimaltay152@hotmail.com>
2021-03-02 23:12:04 +0300

    Added email

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-
-
 from views import run_method_with_perfect_intervals
+from config import send_email
 
 from models import fetch_btc_data_from_binance, create_btc_table_if_not_exists, \
 create_altcoin_table_if_not_exists, fetch_altcoin_data_from_binance
 
-create_btc_table_if_not_exists()
-create_altcoin_table_if_not_exists()
+def main():
+    send_email("Bot is up and running!", "SUCCESS", "xrge152@gmail.com")
 
-run_method_with_perfect_intervals(fetch_btc_data_from_binance, 1)
+    try:
+        print("Application Started!")
+        create_btc_table_if_not_exists()
+        create_altcoin_table_if_not_exists()
+        run_method_with_perfect_intervals(fetch_btc_data_from_binance, 1)
+    except Exception as e:
+        send_email("The application stopped: " + str(e))
 
 
+if __name__ == "__main__":
+    main()
--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 from binance.client import Client
 import sqlite3
 import json
+import smtplib
+from email.message import EmailMessage
 
 json_file = open('secrets.json', 'r')
 parsed_json_file = json.load(json_file)
@@ -11,3 +13,20 @@ api_secret = parsed_json_file.get('API_SECRET')
 client = Client(api_key, api_secret)
 conn = sqlite3.connect('coins.db')
 c = conn.cursor()
+
+def send_email(text, subject, receiver):
+
+    msg = EmailMessage()
+    msg.set_content(text)
+    msg['Subject'] = subject
+    msg['From'] = "TradingBot"
+    msg['To'] = receiver
+
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+
+    email = parsed_json_file.get('EMAIL')
+    email_password = parsed_json_file.get('EMAIL_PASSWORD')
+
+    server.login(email, email_password)
+    server.send_message(msg)
+    server.quit()
