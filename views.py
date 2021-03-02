e0c36c3b53666104f1681f7ad6c05a086510b373
ibrahim altay <ibrahimaltay152@hotmail.com>
2021-03-02 22:26:38 +0300

    Added longer wait times

--- views.py
+++ views.py
@@ -9,5 +9,6 @@ def run_method_with_perfect_intervals(method, interval_seconds):
         
         #print('It took', (timer_stop - timer_start)/1000000, 'miliseconds to run this piece of code.')
         elapsed = (timer_stop - timer_start) / 1000000000
-        time.sleep(interval_seconds - elapsed)
+        if interval_seconds - elapsed > 0:
+            time.sleep(interval_seconds - elapsed)
         #print("Execution time was subtracted from the sleeping time to ensure perfect timing!")
