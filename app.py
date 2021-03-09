6bbdc727144d08cab8375973863a0ff855a4aed5
ibrahim altay <ibrahimaltay152@hotmail.com>
2021-03-10 00:50:18 +0300

    More meaningful emails

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def main():
     create_table_if_not_exists(PARAMETER, conn, c)
     fetch_crypto_data_from_binance(PARAMETER, PARAMETER, conn, c)
     datetime_now = datetime.now().astimezone().strftime("%d/%m/%Y %H:%M:%S")
-    send_email(f"Successful execution at {datetime_now}", MAIL_LIST)
+    send_email(f"{PARAMETER} Successful execution at {datetime_now}", MAIL_LIST)
 
 if __name__ == '__main__':
     try:
